07f4493d74a26edc1d0716d88f555663b1f5c234
ai-push-check <ai-push-check@anthropic.com>
2026-03-02 13:33:19 +0800

    init persion web

---
+++ src/_data/navigation.js
@@ -0,0 +1,34 @@
+export default {
+  top: [
+    {
+      text: '关于',
+      url: '/about/'
+    },
+    {
+      text: '文章',
+      url: '/get-started/'
+    },
+    {
+      text: '博客',
+      url: '/blog/'
+    }
+  ],
+  bottom: [
+    {
+      text: 'Style guide',
+      url: '/styleguide/'
+    },
+    {
+      text: 'Imprint',
+      url: '/imprint/'
+    },
+    {
+      text: 'Privacy',
+      url: '/privacy/'
+    },
+    {
+      text: 'Accessibility',
+      url: '/accessibility/'
+    }
+  ]
+};
